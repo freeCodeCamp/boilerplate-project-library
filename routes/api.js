a576b074fed16dce9f48f433384a6f30481bdcd9
Joao Esperancinha <jofisaes@gmail.com>
2019-05-23 18:51:53 +0100

    requirements complete missing unit tests

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@
 *
 *
 *       Complete the API routing below
-*       
-*       
+*
+*
 */
 
 'use strict';
@@ -11,24 +11,58 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB;
+const mongoose =  require('mongoose');
+const MONGODB_CONNECTION_STRING = process.env.CONNECTION_STRING;
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
-
+mongoose.connect(MONGODB_CONNECTION_STRING);
+const Schema = mongoose.Schema;
+const BookSchema = new Schema({
+  title: String,
+  commentcount: Number,
+  comments:Array
+})
+const Book = mongoose.model('Book', BookSchema);
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
+      Book.find(req.body, (err, data) => {
+        if(err){
+          res.send('no book exists')
+        } else {
+          res.json(data);
+        }
+      });
+    console.log('/api/books');
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
-    
+
     .post(function (req, res){
       var title = req.body.title;
+      let book = new Book({
+        title: title,
+        commentcount: 0
+      });
+      book.save((err, data) => {
+        if(err){
+          res.send('no book exists');
+        } else {
+          res.json(data);
+        }
+      })
       //response will contain new book object including atleast _id and title
     })
-    
+
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+      Book.deleteMany({}, (error, data)=>{
+        if(error){
+          res.send('failed delete all');
+        } else {
+          res.send('complete delete successful');
+        }
+      });
     });
 
 
@@ -36,18 +70,54 @@ module.exports = function (app) {
   app.route('/api/books/:id')
     .get(function (req, res){
       var bookid = req.params.id;
+      Book.findById(bookid)
+        .or(req.body)
+        .exec((err, data) => {
+          if(err || !data){
+            res.send('no book exists')
+          } else {
+            res.json(data);
+          }
+      });
+        console.log('/api/books/:id');
+
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
+
     .post(function(req, res){
       var bookid = req.params.id;
       var comment = req.body.comment;
+      Book.findById(bookid)
+        .exec((err, data) => {
+          if(err){
+            res.send('no book exists')
+          } else {
+            if(!data.comments){
+              data.comments = [];
+            }
+            data.comments.push(comment);
+            data.save((err, savedData)=>{
+              if(err){
+                res.send('no book exists');
+              } else {
+                res.json(savedData);
+              }
+            })
+          }
+      });
       //json res format same as .get
     })
-    
+
     .delete(function(req, res){
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
+      Book.findOneAndRemove({_id: bookid}, (err, data) => {
+        if(err){
+          res.send('no book exists')
+        } else {
+          res.json('delete successful');
+        }
+      })
     });
-  
+
 };
--- server.js
+++ server.js
@@ -7,8 +7,11 @@ var cors        = require('cors');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
-
+const helmet          = require('helmet');
 var app = express();
+
+app.use(helmet.noCache())
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -26,9 +29,9 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
